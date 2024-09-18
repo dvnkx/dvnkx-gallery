f3f56f748cda7dc129efc7bd695286c1c985988a
Bogdan <dvnkxxxx@gmail.com>
2024-09-18 15:26:43 +0300

    add: page header component

--- src/app/(homePage)/page.tsx
+++ src/app/(homePage)/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import GalleryScroll from "./_components/GalleryScroll";
+import PageHeader from "@/components/PageHeader";
 
 const HomePage = () => {
   return (
     <main className="overflow-hidden">
+      <PageHeader>Gallery</PageHeader>
       <GalleryScroll direction="left" />
       <GalleryScroll direction="right" categoryName="Newest" />
     </main>
