d14bc7f59a2b1e5d226639fe4d3c50a4c57f9f0e
Bogdan <dvnkxxxx@gmail.com>
2024-09-19 17:54:41 +0300

    fix: removed unused import

--- src/app/new/category/_actions/addCategory.ts
+++ src/app/new/category/_actions/addCategory.ts
@@ -3,7 +3,6 @@
 import { routes } from "@/keys/routes";
 import prisma from "@/lib/db";
 import { categorySchema } from "@/schemas/category.schema";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { redirect } from "next/navigation";
 
 export const addCategory = async (_: unknown, formData: FormData) => {
