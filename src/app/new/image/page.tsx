a8eafdeb58d9ef83ac0c47bc9eb42d18b20c74aa
Bogdan <dvnkxxxx@gmail.com>
2024-09-19 17:20:14 +0300

    fix: added metadata for new image page

--- src/app/new/image/page.tsx
+++ src/app/new/image/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next/types";
 import PageHeader from "@/components/PageHeader";
 import React from "react";
 import NewImageForm from "./_components/NewImageForm";
+
+export const metadata: Metadata = {
+  title: "Gallery | New Image",
+  description: "Upload new image.",
+};
 
 const NewImagePage = () => {
   return (
