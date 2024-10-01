d243f896fc01c6da62054a8704711d83b4205b94
Bogdan <dvnkxxxx@gmail.com>
2024-10-01 19:33:31 +0300

    fix: minor styles change

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 const PageHeader = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex justify-center">
-      <h1 className="my-5 text-7xl font-kyiv">{children}</h1>
+      <h1 className="my-5 text-7xl font-kyiv tracking-wide">{children}</h1>
     </section>
   );
 };
